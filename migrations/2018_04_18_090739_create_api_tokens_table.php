07e4f3d8177373c9e3b65a278dd85f1a11792afd
Maduka Jayalath <madu.rapa@gmail.com>
2025-06-04 15:44:21 +0530

    Laravel 12's migration convention

--- migrations/2018_04_18_090739_create_api_tokens_table.php
+++ migrations/2018_04_18_090739_create_api_tokens_table.php
@@ -14,7 +14,7 @@ class CreateApiTokensTable extends Migration
     public function up()
     {
         Schema::create('api_tokens', function (Blueprint $table) {
-            $table->increments('id');
+            $table->id();
             $table->string('service');
             $table->string('token');
             $table->timestamps();
